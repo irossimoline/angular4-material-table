2ca562af82905e8bd0728b3a5336bf81d79e0adc
Ignacio Rossi <i.rossi.moline@gmail.com>
2017-10-18 00:16:41 -0300

    fix: exported ValidatorService

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 export { AppModule } from './src/app.module';
+export { ValidatorService } from './src/validator.service';
 export { DefaultValidatorService } from './src/default-validator.service';
 export { TableDataSource } from './src/table-data-source';
 export { TableElement } from './src/table-element';

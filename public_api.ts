97291bc38e2757d86c5a7861b9a83c683b8614d8
Benoit Lavenier <benoit.lavenier@e-is.pro>
2020-09-21 12:45:11 +0200

    Fix build (bad path in public_api.ts)

--- public_api.ts
+++ public_api.ts
@@ -1,9 +1,8 @@
-export * from './src/app.module';
-export * from './src/default-validator.service';
-export * from './src/table-data-source';
-export * from './src/table-element.factory';
-export * from './src/table-element';
-export * from './src/table-element-reactive-forms';
-export * from './src/table-element-template-driven';
-export * from './src/validator.service';
+export * from './src/app/ngx-material-table/default-validator.service';
+export * from './src/app/ngx-material-table/table-data-source';
+export * from './src/app/ngx-material-table/table-element.factory';
+export * from './src/app/ngx-material-table/table-element';
+export * from './src/app/ngx-material-table/table-element-reactive-forms';
+export * from './src/app/ngx-material-table/table-element-template-driven';
+export * from './src/app/ngx-material-table/validator.service';
 
